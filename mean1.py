1367c38309678028ea76e783b3b01871a76ebfd0
Pheotixx <72797783+Pheotixx@users.noreply.github.com>
2021-10-19 20:31:13 +0530

    Update mean1.py

--- mean1.py
+++ mean1.py
@@ -20,3 +20,21 @@ for x in new_Data:
 mean = total/n
 
 print("The average is " + str(mean))
+
+for i in range(len(file_data)):
+    n_num = file_data[i][2]
+    newData.append(float(n_num))
+
+n = len(newData)
+newData.sort()
+
+if n % 2 == 0:
+    median1 = float(newData[n//2])
+    median2 = float(newData[n//2 - 1])
+    median = (median1 + median1)/2
+
+else:
+    median = newData[n//2]
+    print(n)
+
+print("Median is " + str(median))
